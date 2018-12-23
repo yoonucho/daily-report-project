960c05ba28384069814037f7198f37db11c4fc80
yoonucho <yoonu78@naver.com>
2018-12-24 03:27:52 +0900

    수정: 네비메뉴 아이콘추가 및 css수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,51 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { MdHome } from "react-icons/md";
+import { MdDateRange } from "react-icons/md";
+import { MdAssignment } from "react-icons/md";
+import { MdSettings } from "react-icons/md";
+import { MdMood } from "react-icons/md";
 import { Link } from "react-router-dom";
+
 class Nav extends Component {
 	render() {
-		return <p>Nav</p>;
+		return (
+			<Fragment>
+				<nav className="nav">
+					<ul className="nav-list">
+						<li>
+							<Link to="/day">
+								<MdHome />
+								<span>오늘</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/calendar">
+								<MdDateRange />
+								<span>달력</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/search">
+								<MdAssignment />
+								<span>기록</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/setting">
+								<MdSettings />
+								<span>설정</span>
+							</Link>
+						</li>
+						<li>
+							<Link to="/about">
+								<MdMood />
+								<span>소개</span>
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</Fragment>
+		);
 	}
 }
 export default Nav;
