f239c5c38cf828d7a2f4b321948003e83237f0b7
yoonucho <yoonu78@naver.com>
2019-01-17 16:15:54 +0900

    수정: unit test코드 수정

--- backend/server.test.js
+++ backend/server.test.js
@@ -1,23 +1,25 @@
-const supertest = require('supertest');
-const server = require('./server');
-const mongoose = require('mongoose');
+const request = require("supertest");
+const server = require("./server");
+// const mongoose = require("mongoose");
 
-//console.log(server);
+console.dir(server);
 
-describe('server', () => {
-	it('test', () => {
-		// request(todayRoutes).get('/').then((response) => {
-		// 	expect(response.statusCode).toBe(200);
-		// 	done();
-		// })
-		return supertest(server).get('/').expect(200);
+describe("Test the root path", () => {
+	test("It should response the Get method", () => {
+		// request(server)
+		// 	.get("/")
+		// 	.then(response => {
+		// 		expect(response.statusCode).toBe(200);
+		// 		done();
+		// 	});
+		return request(server)
+			.get("/")
+			.expect(200);
 	});
 
-	beforeAll(() => {
-		mongoose.connect(
-			'mongodb://127.0.0.1:27017/today'
-
-		);
+	// beforeAll(() => {
+	// 	mongoose.connect("mongodb://127.0.0.1:27017/today");
+	// });
 
 	// afterAll(done => {
 	// 	mongoose.disconnect(done);
