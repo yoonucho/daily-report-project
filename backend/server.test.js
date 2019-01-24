ebd438ef535856ecdfd562072d0e87a7dd858f47
yoonucho <yoonu78@naver.com>
2019-01-25 00:08:16 +0900

    Update server.test.js

--- backend/server.test.js
+++ backend/server.test.js
@@ -18,21 +18,10 @@ describe('server', () => {
 			'mongodb://127.0.0.1:27017/today'
 
 		);
-<<<<<<< HEAD
+
 	});
 	afterAll((done) => {
 		mongoose.disconnect(done);
 	});
 
-
-
-})
-
-
-=======
-
-	// afterAll(done => {
-	// 	mongoose.disconnect(done);
-	// });
 });
->>>>>>> 2b0b5aa9d83dbf489f5358a5a8a60c50644e16a5
