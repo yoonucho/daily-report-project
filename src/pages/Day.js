c78d1c54b77bf4812670fa14bae0d66628ea30d9
yoonucho <yoonu78@naver.com>
2019-03-10 03:01:37 +0900

    수정: Day.js 클래스를 함수로 변경 중

--- src/pages/Day.js
+++ src/pages/Day.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 // import moment from 'moment';
 import DayList from "../components/DayList";
@@ -8,6 +8,7 @@ import DayScore from "../components/DayScore";
 const oneDay = new Array(25).fill(0).map((key, index) => ({ id: index, action: `test${index}`, time: `${index}` }));
 
 const Day = () => {
+
 	const [list, setList] = useState({ oneday })
 
 	//componentDidMount(), componentDidUpdate()와 비슷
@@ -51,57 +52,16 @@ const Day = () => {
 		});
 	};
 
-	// 저장버튼 눌렀을때
-	// handleSubmit = e => {
-	// 	// 페이지 리로딩 방지
-	// 	e.preventDefault();
-	// 	console.log("handleSubmit")
-	// 	// e.preventDefault();
-	// 	// this.setState(({ list, action, id }) => ({
-	// 	// 	list: [
-	// 	// 		...list,
-	// 	// 		{
-	// 	// 			id,
-	// 	// 			action
-	// 	// 		}
-	// 	// 	],
-	// 	// 	action
-
-	// 	// }));
-
-	// }
-
 	console.log("today");
 	return (
-		<Fragment>
-			<div className="day">
-				<h1 className="day-title">title</h1>
-				<DayList list={list} onChange={this.handleChange} />
-				<DayScore />
-				{/* <li className="day-list-item">
-							<div className="time">
-								<h2>time</h2>
-							</div>
-							<form className="action">
-								<input
-									type="text"
-									value={this.state.action}
-									placeholder="내용을 작성해주세요."
-									onChange={this.handleChage}
-								/>
-							
-							
-								<div className="buttons">
-									<button type="submit" className="save"
-										onClick={this.handleSubmit}>저장
-									</button>
-									<button className="cancel">취소</button>
-								</div>
-							</form>
-						</li> */}
-			</div>
-		</Fragment>
+		<div className="day">
+			<h1 className="day-title">title</h1>
+			{/* 이게 가능한것인가? */}
+			<DayList list={list} onChange={this.handleChange} />
+			<DayScore />
+		</div>
 	)
+
 }
 
 export default Day;
