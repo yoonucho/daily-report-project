cd3a175aae93059ed77e2e48c5bd775cd201ebd8
yoonucho <yoonu78@naver.com>
2019-03-11 23:29:56 +0900

    수정: 코드 리팩토링중

--- src/pages/Day.js
+++ src/pages/Day.js
@@ -5,59 +5,42 @@ import DayList from "../components/DayList";
 import DayScore from "../components/DayScore";
 
 
-const oneDay = new Array(25).fill(0).map((key, index) => ({ id: index, action: `test${index}`, time: `${index}` }));
 
 const Day = () => {
 
-	const [list, setList] = useState({ oneday })
-
-	//componentDidMount(), componentDidUpdate()와 비슷
-
-	useEffect(() => {
-		(async function () {
-			try {
-				const response = await axios.get('http://localhost:4000/today/2019-02-11');
-				list({ list: response.data });
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, []);
-
-	// 업데이트 될 때
-	// handleUpdate = () => {
-	// 	this.setState(({ list, action, id }) => ({
-	// 		list: [
-	// 			...list,
-	// 			{
-	// 				id,
-	// 				action
-	// 			}
-	// 		],
-
-	// 	}));
-
-	// }
-
-
-
-
+	const oneDay = new Array(25).fill(0).map((key, index) => ({ id: index, action: `test${index}`, time: `${index}` }));
+	const [list, setList] = useState(oneday);
 
 	// 인풋 클릭했을때
 	handleChange = e => {
 		// 페이지 리로딩 방지
 		e.preventDefault();
-		setList({
-			action: e.target.value
-		});
+		setList(
+			e.target.value
+		);
 	};
+
+	const getUrl = async () => {
+		try {
+			const response = await axios.get('http://localhost:4000/today/2019-02-11');
+			list({ list: response.data });
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	//componentDidMount(), componentDidUpdate()와 비슷
+	useEffect(() => {
+		getUrl();
+	}, []);
 
 	console.log("today");
 	return (
 		<div className="day">
 			<h1 className="day-title">title</h1>
 			{/* 이게 가능한것인가? */}
-			<DayList list={list} onChange={this.handleChange} />
+			<DayList list={list} onChange={handleChange} />
+			{/* <input value={action} onChange={this.handleChange} /> */}
 			<DayScore />
 		</div>
 	)
