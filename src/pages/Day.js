c9400744bb80221224fa30773d29b3f22a5ab059
yoonucho <yoonu78@naver.com>
2019-03-12 23:27:38 +0900

    수정: Day.js 리팩토링

--- src/pages/Day.js
+++ src/pages/Day.js
@@ -1,107 +1,49 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 // import moment from 'moment';
 import DayList from "../components/DayList";
 import DayScore from "../components/DayScore";
 
 
-const oneDay = new Array(25).fill(0).map((key, index) => ({ id: index, action: `test${index}`, time: `${index}` }));
 
-class Day extends Component {
-	state = {
-		list: oneDay
+const Day = () => {
+	const oneDay = new Array(25).fill(0).map((key, index) => ({ id: index, action: `test${index}`, time: `${index}` }));
+	const [list, setList] = useState(oneDay);
+	console.log(oneDay)
+	// state = {
+	// 	list: oneDay
 
-	};
+	// };
 
-
-
-	// 업데이트 될 때
-	// handleUpdate = () => {
-	// 	this.setState(({ list, action, id }) => ({
-	// 		list: [
-	// 			...list,
-	// 			{
-	// 				id,
-	// 				action
-	// 			}
-	// 		],
-
-	// 	}));
-
-	// }
-
-	async componentDidMount() {
+	const getUrl = async () => {
 		try {
 			const response = await axios.get('http://localhost:4000/today/2019-02-11');
-			this.setState({ list: response.data });
+			list({ list: response.data });
 		} catch (error) {
 			console.log(error);
 		}
-
 	}
+
+	//componentDidMount(), componentDidUpdate()와 비슷
+	useEffect(() => {
+		getUrl();
+	}, []);
 
 	// 인풋 클릭했을때
-	handleChange = e => {
-		// 페이지 리로딩 방지
-		e.preventDefault();
-		this.setState({
-			action: e.target.value
-		});
-	};
+	// handleChange = e => {
+	// 	// 페이지 리로딩 방지
+	// 	e.preventDefault();
+	// 	setList(e.target.value);
+	// };
 
-	// 저장버튼 눌렀을때
-	handleSubmit = e => {
-		// 페이지 리로딩 방지
-		e.preventDefault();
-		console.log("handleSubmit")
-		// e.preventDefault();
-		// this.setState(({ list, action, id }) => ({
-		// 	list: [
-		// 		...list,
-		// 		{
-		// 			id,
-		// 			action
-		// 		}
-		// 	],
-		// 	action
-
-		// }));
-
-	};
-
-	render() {
-		console.log("today");
-		return (
-			<Fragment>
-				<div className="day">
-					<h1 className="day-title">title</h1>
-					<DayList list={this.state.list} onChange={this.handleChange} />
-					<DayScore />
-					{/* <li className="day-list-item">
-							<div className="time">
-								<h2>time</h2>
-							</div>
-							<form className="action">
-								<input
-									type="text"
-									value={this.state.action}
-									placeholder="내용을 작성해주세요."
-									onChange={this.handleChage}
-								/>
-							
-							
-								<div className="buttons">
-									<button type="submit" className="save"
-										onClick={this.handleSubmit}>저장
-									</button>
-									<button className="cancel">취소</button>
-								</div>
-							</form>
-						</li> */}
-				</div>
-			</Fragment>
-		)
-	}
+	console.log("today");
+	return (
+		<div className="day">
+			<h1 className="day-title">title</h1>
+			{/* <DayList list={list} onChange={this.handleChange}/> */}
+			<DayScore />
+		</div>
+	)
 }
 
 export default Day;
