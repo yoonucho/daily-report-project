bdbd3f0f25a4affce7a2cd2e3c987b7da50ccc0b
yoonucho <yoonu78@naver.com>
2019-03-17 14:52:57 +0900

    수정:Day.js 충돌코드 수정

--- src/pages/Day.js
+++ src/pages/Day.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
-<<<<<<< HEAD
-import axios from 'axios';
-=======
 import axios from "axios";
->>>>>>> 77118eafe27b80d2f0f80b816a14667d59247803
 // import moment from 'moment';
 import DayList from "../components/DayList";
 import DayScore from "../components/DayScore";
@@ -17,19 +13,12 @@ const Day = () => {
 
 	const [list, setList] = useState(oneDay);
 
-<<<<<<< HEAD
-const Day = () => {
-
-	const [list, setList] = useState({ oneday })
-
-=======
 	console.log(oneDay);
 	// state = {
 	// 	list: oneDay
 
 	// };
 
->>>>>>> 77118eafe27b80d2f0f80b816a14667d59247803
 	const getUrl = async () => {
 		try {
 			const response = await axios.get(
@@ -39,24 +28,6 @@ const Day = () => {
 		} catch (error) {
 			console.log(error);
 		}
-<<<<<<< HEAD
-	}
-	//componentDidMount(), componentDidUpdate()와 비슷
-	useEffect(() => {
-		getUrl();
-	}, []);
-
-
-	// 인풋 클릭했을때
-	handleChange = e => {
-		// 페이지 리로딩 방지
-		e.preventDefault();
-		setList({
-			action: e.target.value
-		});
-	};
-
-=======
 	};
 
 	//componentDidMount(), componentDidUpdate()와 비슷
@@ -69,25 +40,14 @@ const Day = () => {
 		setList(e.target.value);
 	};
 
->>>>>>> 77118eafe27b80d2f0f80b816a14667d59247803
 	console.log("today");
 	return (
 		<div className="day">
 			<h1 className="day-title">title</h1>
-<<<<<<< HEAD
-			{/* 이게 가능한것인가? */}
-			<DayList list={list} onChange={this.handleChange} />
-			<DayScore />
-		</div>
-	)
-
-}
-=======
 			<DayList list={list} onChange={handleChange} />
 			<DayScore />
 		</div>
 	);
 };
->>>>>>> 77118eafe27b80d2f0f80b816a14667d59247803
 
 export default Day;
